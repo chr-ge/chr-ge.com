e9daf5120aa3113928650a98eeb063ff14d4cab7
chr-ge <ace26@protonmail.com>
2021-04-28 16:34:38 -0400

    feat: add plausible.io analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,24 @@
 import type { AppProps } from 'next/app'
+import Head from "next/head"
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <meta content='width=device-width, initial-scale=1' name='viewport' />
+        {process.env.NODE_ENV === 'production' && (
+          <script
+            async
+            defer
+            data-domain='chr-ge.com'
+            src='https://plausible.io/js/plausible.js'
+          />
+        )}
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp

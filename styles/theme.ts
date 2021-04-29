2c47a9a6954b93dd9aa7986c416b2ffd43221218
chr-ge <ace26@protonmail.com>
2021-04-29 19:58:52 -0400

    feat(theme): add custom color palette

--- styles/theme.ts
+++ styles/theme.ts
@@ -3,6 +3,47 @@ import { extendTheme } from '@chakra-ui/react'
 const theme = extendTheme({
   colors: {
     black: '#16161D',
+    primary: {
+      100: '#AAEFF0',
+      200: '#77E5E7',
+      300: '#55DFE1',
+      400: '#33D8DB',
+      500: '#22c1c3',
+      600: '#1EA5A7',
+      700: '#19898B',
+      800: '#146E6F',
+      900: '#0C4546',
+    },
+    secondary: {
+      100: '#FEE6B3',
+      200: '#FED886',
+      300: '#FDCE68',
+      400: '#FDC449',
+      500: '#fdbb2d',
+      600: '#FBAC03',
+      700: '#D19002',
+      800: '#A77302',
+    },
+  },
+  fonts: {
+    heading: 'Mulish, sans-serif',
+    body: 'Mulish, sans-serif',
+  },
+  shadows: {
+    secondary:
+      '0 0 1px 2px rgba(253, 196, 73, .75), 0 1px 1px rgba(0, 0, 0, .15)',
+  },
+  components: {
+    Button: {
+      variants: {
+        'icon-button': ({ colorMode }) => ({
+          bg: colorMode === 'dark' ? 'gray.700' : 'gray.100',
+          _focus: {
+            boxShadow: 'secondary',
+          },
+        }),
+      },
+    },
   },
 })
 

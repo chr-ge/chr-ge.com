a3110622bda08bfe10e83926a1550cf74d497c05
chr-ge <ace26@protonmail.com>
2021-04-29 16:41:45 -0400

    feat: add i18n

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,13 @@
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+
+export const getStaticProps = async ({ locale }: { locale: string }) => ({
+  props: {
+    ...await serverSideTranslations(locale, ['common']),
+  }
+})
 
 export default function Home() {
   return (

d6544a3b3db9ceeeb5f221396fa571b9f40caed6
chr-ge <ace26@protonmail.com>
2021-05-03 00:21:12 -0400

    feat(Menu): add background opacity when open

--- components/Menu.tsx
+++ components/Menu.tsx
@@ -1,7 +1,7 @@
 import { FC } from 'react'
 import { useRouter } from 'next/router'
 import { useTranslation } from 'next-i18next'
-import { Button, Icon, Portal, SimpleGrid } from '@chakra-ui/react'
+import { Box, Button, Icon, Portal, SimpleGrid } from '@chakra-ui/react'
 import { IoHome } from 'react-icons/io5'
 import { FaCode } from 'react-icons/fa'
 import { CgWebsite } from 'react-icons/cg'
@@ -13,35 +13,44 @@ const Menu: FC = () => {
 
   return (
     <Portal>
-      <SimpleGrid
+      <Box
+        bgColor='rgba(0,0,0,0.4)'
         position='absolute'
-        right={0}
+        top={0}
         bottom={0}
-        margin='8'
-        padding='4'
-        bg='white'
-        boxShadow='xl'
-        borderRadius='lg'
-        columns={2}
-        gap='2'
+        right={0}
+        left={0}
       >
-        <Button aria-label={t('home')} variant='menu' type='button'>
-          <Icon as={IoHome} boxSize='6' mb='1' />
-          {t('home')}
-        </Button>
-        <Button aria-label={t('projects')} variant='menu' type='button'>
-          <Icon as={FaCode} boxSize='6' mb='1' />
-          {t('projects')}
-        </Button>
-        <Button aria-label='Blog' variant='menu' type='button'>
-          <Icon as={CgWebsite} boxSize='6' mb='1' />
-          Blog
-        </Button>
-        <Button aria-label={t('contact')} variant='menu' type='button'>
-          <Icon as={AiOutlineMessage} boxSize='6' mb='1' />
-          {t('contact')}
-        </Button>
-      </SimpleGrid>
+        <SimpleGrid
+          position='fixed'
+          right={0}
+          bottom={0}
+          margin='8'
+          padding='4'
+          bg='white'
+          boxShadow='xl'
+          borderRadius='lg'
+          columns={2}
+          gap='2'
+        >
+          <Button aria-label={t('home')} variant='menu' type='button'>
+            <Icon as={IoHome} boxSize='6' mb='1' />
+            {t('home')}
+          </Button>
+          <Button aria-label={t('projects')} variant='menu' type='button'>
+            <Icon as={FaCode} boxSize='6' mb='1' />
+            {t('projects')}
+          </Button>
+          <Button aria-label='Blog' variant='menu' type='button'>
+            <Icon as={CgWebsite} boxSize='6' mb='1' />
+            Blog
+          </Button>
+          <Button aria-label={t('contact')} variant='menu' type='button'>
+            <Icon as={AiOutlineMessage} boxSize='6' mb='1' />
+            {t('contact')}
+          </Button>
+        </SimpleGrid>
+      </Box>
     </Portal>
   )
 }

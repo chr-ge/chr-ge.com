2274c828f8aee506a1601f23e330bf5d7ddaf264
chr-ge <ace26@protonmail.com>
2021-05-08 00:14:06 -0400

    feat(api): create contact endpoint

---
+++ pages/api/contact.ts
@@ -0,0 +1,38 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import nodemailer from 'nodemailer'
+import { isEmail, emailText, emailTemplate } from 'utils/email'
+
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  const { email, message } = req.body
+  const userAgent = req.headers['user-agent'] ?? ''
+
+  if (!email || !isEmail(email) || !message) {
+    return res
+      .status(400)
+      .json({ success: false, message: 'Malformed content' })
+  }
+
+  const transporter = nodemailer.createTransport({
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true,
+    auth: {
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASS,
+    },
+  })
+
+  const mailData = {
+    from: process.env.EMAIL_USER,
+    to: process.env.EMAIL_TO,
+    subject: `CHR-GE // Message from ${email}`,
+    text: emailText(email, message, userAgent),
+    html: emailTemplate(email, message, userAgent),
+  }
+
+  transporter.sendMail(mailData, (err) => {
+    if (err) console.log(err)
+  })
+
+  res.status(200).json({ success: true })
+}

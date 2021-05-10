8a21563ed4d97743155575de60de23f349e12871
George <61952924+chr-ge@users.noreply.github.com>
2021-05-10 09:23:45 -0400

    Update index.ts

--- components/Icons/index.ts
+++ components/Icons/index.ts
@@ -1,4 +1,5 @@
 export { default as ChakraUI } from './chakraui'
+export { default as DynamoDB } from './dynamodb'
 export { default as Express } from './express'
 export { default as Javascript } from './javascript'
 export { default as Vercel } from './vercel'

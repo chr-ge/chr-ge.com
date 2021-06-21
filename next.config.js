2f09efdbd68e9c7cb498d00e42a204750c7cde18
chr-ge <ace26@protonmail.com>
2021-06-21 19:50:48 -0400

    feat: add ts-check

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 const { i18n } = require('./next-i18next.config')
 
-module.exports = {
+// @ts-check
+
+/**
+ * @type {import('next/dist/next-server/server/config').NextConfig}
+ **/
+const nextConfig = {
   reactStrictMode: true,
   i18n,
 }
+
+module.exports = nextConfig

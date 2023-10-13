a9b3073cdd611135485e09f6d198c4fcc67f3416
chr-ge <ace26@protonmail.com>
2023-10-13 18:36:52 -0400

    chore: change header ease

--- components/structure/Header/Menu.tsx
+++ components/structure/Header/Menu.tsx
@@ -16,34 +16,32 @@ interface MenuProps {
   onClose: () => void
 }
 
-export const Menu: React.FC<MenuProps> = ({ isOpen, onClose }) => {
-  return (
-    <AnimatePresence initial={false}>
-      {isOpen && (
-        <MotionStack
-          key='header-menu'
-          as='nav'
-          pos='absolute'
-          zIndex='sticky'
-          left='0'
-          right='0'
-          py='4'
-          px={{ base: '4', md: '8' }}
-          direction={{ base: 'column', md: 'row' }}
-          spacing='4'
-          borderColor='black'
-          borderBottom='1px solid'
-          bgGradient='linear(to-r, #DFE9F3, white)'
-          initial={{ opacity: 0, top: '0rem' }}
-          animate={{ opacity: 1, top: 'var(--chakra-sizes-header-height)' }}
-          exit={{ opacity: 0, top: '0rem' }}
-          transition={{ ease: 'backOut' }}
-        >
-          {navItems.map((item) => (
-            <NavItem key={item.name} onClose={onClose} {...item} />
-          ))}
-        </MotionStack>
-      )}
-    </AnimatePresence>
-  )
-}
+export const Menu: React.FC<MenuProps> = ({ isOpen, onClose }) => (
+  <AnimatePresence initial={false}>
+    {isOpen && (
+      <MotionStack
+        key='header-menu'
+        as='nav'
+        pos='absolute'
+        zIndex='sticky'
+        left='0'
+        right='0'
+        py='4'
+        px={{ base: '4', md: '8' }}
+        direction={{ base: 'column', md: 'row' }}
+        spacing='4'
+        borderColor='black'
+        borderBottom='1px solid'
+        bgGradient='linear(to-r, #DFE9F3, white)'
+        initial={{ opacity: 0, top: '0rem' }}
+        animate={{ opacity: 1, top: 'var(--chakra-sizes-header-height)' }}
+        exit={{ opacity: 0, top: '0rem' }}
+        transition={{ ease: 'circOut' }}
+      >
+        {navItems.map((item) => (
+          <NavItem key={item.name} onClose={onClose} {...item} />
+        ))}
+      </MotionStack>
+    )}
+  </AnimatePresence>
+)

7d790bd3fba19cd02f43c9ec060a3d95a32567e2
chr-ge <ace26@protonmail.com>
2023-10-22 20:00:47 -0400

    chore: remove allowEmpty

--- src/config/browser.env.ts
+++ src/config/browser.env.ts
@@ -1,9 +1,7 @@
 import { envsafe, str, url } from 'envsafe'
-import { config } from './config'
 
 export const env = envsafe({
   NEXT_PUBLIC_CLOUDFLARE_TOKEN: str({
-    allowEmpty: !config.isProd,
     input: process.env.NEXT_PUBLIC_CLOUDFLARE_TOKEN,
   }),
   NEXT_PUBLIC_FORMSPREE_URL: url({
